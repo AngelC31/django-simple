09673ff325b21033ad2701ba2a07cce004aebf25
AngelC31 <85770415+AngelC31@users.noreply.github.com>
2024-10-28 12:52:14 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f%_!np%rz7e-j^eucnxkq0ij887o%&%trfaj00mq*2_sris$@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['prueba.ludensprod.com', 'localhost:3000']
 
 
 # Application definition
